f7b2fc3fc7d3c5d0cee2032b68c92e2a01cddcaf
Jeswin <jeswinpk@agilehead.com>
2026-09-20 20:16:30 +0530

    Checkpoint native allocation improvements; certification pending

--- csharp/src/Tsonic.CSharp.Node/fs/metadata/stat.cs
+++ csharp/src/Tsonic.CSharp.Node/fs/metadata/stat.cs
@@ -11,39 +11,6 @@ public static partial class fs
     /// <returns>A promise that resolves to a Stats object.</returns>
     public static Task<Stats> stat(string path)
     {
-        return Task.Run(() =>
-        {
-            var fileInfo = new FileInfo(path);
-            var dirInfo = new DirectoryInfo(path);
-
-            var isFile = fileInfo.Exists;
-            var isDir = dirInfo.Exists;
-
-            if (!isFile && !isDir)
-            {
-                throw new FileNotFoundException($"No such file or directory: {path}");
-            }
-
-            if (isFile)
-            {
-                return new Stats(fileInfo.LastAccessTime, fileInfo.LastWriteTime, fileInfo.CreationTime, fileInfo.CreationTime)
-                {
-                    size = fileInfo.Length,
-                    mode = 0,
-                    isFile = true,
-                    isDirectory = false
-                };
-            }
-            else
-            {
-                return new Stats(dirInfo.LastAccessTime, dirInfo.LastWriteTime, dirInfo.CreationTime, dirInfo.CreationTime)
-                {
-                    size = 0,
-                    mode = 0,
-                    isFile = false,
-                    isDirectory = true
-                };
-            }
-        });
+        return Task.Run(() => statSync(path));
     }
 }
